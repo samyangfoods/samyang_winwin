d6031de12fbc42d2375f68ff9330223f6b29ae95
Hyeonsang <250wons@gmail.com>
2022-01-29 12:11:05 +0900

    refactoring and thumnail basic form added

--- src/components/ImageAccess.js
+++ src/components/ImageAccess.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components/native";
 import * as ImagePicker from "expo-image-picker";
+import ImageAddButton from "./ImageAddButton";
 
 // 2022-01-28 현재 개략적인 프레임만 작성 완료. ✅
 // 이미지가 각각 Hooks 에 담길 수 있도록 image 변수를 세분화 해야 함. ✅
 // image 를 배열로 받아서 처리하는 방법이 좋아보임. ( image = [pic1, pic2, pic3 ....] ) ✅
 
 // 등록 이후 sorting
-// 이미지를 추가했을 때 배열 handling
+// 이미지를 추가했을 때 배열 handling ✅
 
 // 등록된 이미지를 취소하거나 변경할 경우의 로직도 필요.
 // 현재 로직은 부족함 --> ex) "이미지 1" 을 두 번 터치할 경우 이미지가 각각 저장될 것으로 사료됨. 변경이나 삭제가 일어나지 않음.
@@ -27,9 +28,7 @@ function ImageAccess({ image, setImage }) {
   };
 
   const handleArray = (index, uri) => {
-    console.log("start🔥", imageObj);
     const newArr = imageObj.filter((res) => res !== image[index]);
-    console.log("newArr Before🔥", newArr);
     newArr.push(uri);
     console.log("newArr After🔥", newArr);
     setImage(newArr);
@@ -59,44 +58,15 @@ function ImageAccess({ image, setImage }) {
 
   useEffect(() => {
     setImageObj();
-    console.log("useEffect✅✅✅", image);
   }, [imageObj]);
 
   return (
     <Container>
       <HorizontalDiv>
-        <Btn
-          onPress={() => accessAlbum(0)}
-          style={{
-            backgroundColor: image[0] ? "#aaa" : "null",
-          }}
-        >
-          <Text>{image[0] ? image[0].toString().slice(0, 5) : "이미지1"}</Text>
-        </Btn>
-        <Btn
-          onPress={() => accessAlbum(1)}
-          style={{
-            backgroundColor: image[1] ? "#aaa" : "null",
-          }}
-        >
-          <Text>{image[1] ? image[1].toString().slice(0, 5) : "이미지2"}</Text>
-        </Btn>
-        <Btn
-          onPress={() => accessAlbum(2)}
-          style={{
-            backgroundColor: image[2] ? "#aaa" : "null",
-          }}
-        >
-          <Text>{image[2] ? image[2].toString().slice(0, 5) : "이미지3"}</Text>
-        </Btn>
-        <Btn
-          onPress={() => accessAlbum(3)}
-          style={{
-            backgroundColor: image[3] ? "#aaa" : "null",
-          }}
-        >
-          <Text>{image[3] ? image[3].toString().slice(0, 5) : "이미지4"}</Text>
-        </Btn>
+        <ImageAddButton index={0} image={image} accessAlbum={accessAlbum} />
+        <ImageAddButton index={1} image={image} accessAlbum={accessAlbum} />
+        <ImageAddButton index={2} image={image} accessAlbum={accessAlbum} />
+        <ImageAddButton index={3} image={image} accessAlbum={accessAlbum} />
       </HorizontalDiv>
     </Container>
   );
@@ -110,13 +80,4 @@ const HorizontalDiv = styled.View`
   flex-direction: row;
   justify-content: space-between;
   margin: 3% 0;
-`;
-const Text = styled.Text`
-  font-size: 16px;
-`;
-const Btn = styled.TouchableOpacity`
-  margin: 2% 0;
-  padding: 2%;
-  border: 1px solid black;
-  border-radius: 6px;
 `;
