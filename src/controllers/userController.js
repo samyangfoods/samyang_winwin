526afcaaceb5b0ee1f59efba6a8a5fa7903c7c5a
moses1206 <33392504+moses1206@users.noreply.github.com>
2022-04-30 21:58:58 +0900

    2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,18 +1,17 @@
-import mongoose from "mongoose";
-import expressAsyncHandler from "express-async-handler";
-import { User } from "../models/User.js";
-import { Promotion } from "../models/Promotion.js";
-import generateToken from "../utils/generateToken.js";
-import { s3, getSignedUrl } from "../../aws.js";
+import mongoose from 'mongoose'
+import expressAsyncHandler from 'express-async-handler'
+import { User } from '../models/User.js'
+import { Promotion } from '../models/Promotion.js'
+import generateToken from '../utils/generateToken.js'
+import { s3, getSignedUrl } from '../../aws.js'
 
 // @desc    Auth user & get token
 // @route   POST   /api/user/login
 // @access  Private
 const authUser = expressAsyncHandler(async (req, res) => {
-  const { userId, password } = req.body;
+  const { userId, password } = req.body
 
-  const user = await User.findOne({ userId });
-  console.log(user);
+  const user = await User.findOne({ userId })
 
   if (user && (await user.matchPassword(password))) {
     return res.json({
@@ -22,29 +21,29 @@ const authUser = expressAsyncHandler(async (req, res) => {
       storeName: user.storeName,
       role: user.role,
       token: generateToken(user._id),
-    });
+    })
   } else {
-    res.status(401);
-    throw new Error("Invalid userId or password");
+    res.status(401)
+    throw new Error('Invalid userId or password')
   }
-});
+})
 
 const preSigned = expressAsyncHandler(async (req, res) => {
-  console.log(req.body);
+  console.log(req.body)
 
-  const { name } = req.body.objForPreSigned;
-  console.log("Name", name);
+  const { name } = req.body.objForPreSigned
+  console.log('Name', name)
 
-  const imageKey = name;
-  const key = `raw/${imageKey}`;
+  const imageKey = name
+  const key = `raw/${imageKey}`
 
-  const presigned = await getSignedUrl({ key });
+  const presigned = await getSignedUrl({ key })
 
-  console.log(imageKey);
-  console.log(key);
-  console.log(presigned);
-  return res.json({ imageKey, presigned });
-});
+  console.log(imageKey)
+  console.log(key)
+  console.log(presigned)
+  return res.json({ imageKey, presigned })
+})
 
 // const presignedData = await Promise.all(
 //   name.map(async (imagefile, index) => {
@@ -69,32 +68,32 @@ const registerUser = expressAsyncHandler(async (req, res) => {
     phoneNumber,
     userAddress,
     role,
-  } = req.body;
+  } = req.body
 
-  console.log("Register - req.file : ", req.file);
+  console.log('Register - req.file : ', req.file)
 
-  let userImage = "";
+  let userImage = ''
 
   if (req.file) {
-    userImage = req.file.key.replace("raw/", "");
+    userImage = req.file.key.replace('raw/', '')
   } else {
-    userImage = "";
+    userImage = ''
   }
 
-  if (!userId) return res.status(400).send({ err: "userId is required" });
-  if (!password) return res.status(400).send({ err: "password is required" });
-  if (!channel) return res.status(400).send({ err: "channel is required" });
-  if (!userName) return res.status(400).send({ err: "userName is required" });
-  if (!storeName) return res.status(400).send({ err: "storeName is required" });
+  if (!userId) return res.status(400).send({ err: 'userId is required' })
+  if (!password) return res.status(400).send({ err: 'password is required' })
+  if (!channel) return res.status(400).send({ err: 'channel is required' })
+  if (!userName) return res.status(400).send({ err: 'userName is required' })
+  if (!storeName) return res.status(400).send({ err: 'storeName is required' })
   if (!phoneNumber)
-    return res.status(400).send({ err: "phoneNumber is required" });
+    return res.status(400).send({ err: 'phoneNumber is required' })
   if (!userAddress)
-    return res.status(400).send({ err: "userAddress is required" });
+    return res.status(400).send({ err: 'userAddress is required' })
 
-  const userExists = await User.findOne({ userId });
+  const userExists = await User.findOne({ userId })
 
   if (userExists) {
-    return res.status(400).send({ err: "User already exists" });
+    return res.status(400).send({ err: 'User already exists' })
   }
 
   const user = await User.create({
@@ -107,7 +106,7 @@ const registerUser = expressAsyncHandler(async (req, res) => {
     phoneNumber,
     userAddress,
     role,
-  });
+  })
 
   if (user) {
     res.status(201).json({
@@ -120,44 +119,44 @@ const registerUser = expressAsyncHandler(async (req, res) => {
       userAddress: user.userAddress,
       role: user.role,
       token: generateToken(user._id),
-    });
+    })
   } else {
-    res.status(400);
-    throw new Error("Invalid user data");
+    res.status(400)
+    throw new Error('Invalid user data')
   }
-});
+})
 
 // @desc    Get Users
 // @route   GET   /api/user
 // @access  Private
 const getUsers = expressAsyncHandler(async (req, res) => {
-  const users = await User.find({});
-  return res.send({ users });
-});
+  const users = await User.find({})
+  return res.send({ users })
+})
 
 // @desc    Get single User
 // @route   GET   /api/user/:id
 // @access  Private
 const getUser = expressAsyncHandler(async (req, res) => {
-  const { userId } = req.params;
+  const { userId } = req.params
 
   if (!mongoose.isValidObjectId(userId))
-    return res.status(400).send({ err: "invalid params userId" });
+    return res.status(400).send({ err: 'invalid params userId' })
 
-  const user = await User.findOne({ _id: userId });
-  return res.send({ user });
-});
+  const user = await User.findOne({ _id: userId })
+  return res.send({ user })
+})
 
 // @desc    Put single User
 // @route   PUT   /api/user/:id
 // @access  Private
 const updateUser = expressAsyncHandler(async (req, res) => {
-  console.log(req.user);
-  const userId = req.user._id;
-  console.log("유저아이디", userId);
+  console.log(req.user)
+  const userId = req.user._id
+  console.log('유저아이디', userId)
 
   const { channel, userName, storeName, phoneNumber, userAddress, role } =
-    req.body;
+    req.body
 
   // type Validation
   if (
@@ -169,30 +168,30 @@ const updateUser = expressAsyncHandler(async (req, res) => {
     !role
   )
     return res.send({
-      err: "channel or userName or storeName or address or phoneNumber or userImage or role is required !!",
-    });
+      err: 'channel or userName or storeName or address or phoneNumber or userImage or role is required !!',
+    })
 
-  let userImage = "";
+  let userImage = ''
 
-  console.log("🔥🔥🔥", req.file);
+  console.log('🔥🔥🔥', req.file)
   if (req.file) {
-    userImage = req.file.key.replace("raw/", "");
+    userImage = req.file.key.replace('raw/', '')
   } else {
-    userImage = "";
+    userImage = ''
   }
 
-  if (channel && typeof channel !== "string")
-    return res.status(400).send({ err: "channel must be String" });
-  if (userName && typeof userName !== "string")
-    return res.status(400).send({ err: "userName must be String" });
-  if (storeName && typeof storeName !== "string")
-    return res.status(400).send({ err: "storeName must be String" });
-  if (userAddress && typeof userAddress !== "string")
-    return res.status(400).send({ err: "address must be String" });
-  if (phoneNumber && typeof phoneNumber !== "string")
-    return res.status(400).send({ err: "phoneNumber must be String" });
-  if (role && typeof role !== "string")
-    return res.status(400).send({ err: "role must be String" });
+  if (channel && typeof channel !== 'string')
+    return res.status(400).send({ err: 'channel must be String' })
+  if (userName && typeof userName !== 'string')
+    return res.status(400).send({ err: 'userName must be String' })
+  if (storeName && typeof storeName !== 'string')
+    return res.status(400).send({ err: 'storeName must be String' })
+  if (userAddress && typeof userAddress !== 'string')
+    return res.status(400).send({ err: 'address must be String' })
+  if (phoneNumber && typeof phoneNumber !== 'string')
+    return res.status(400).send({ err: 'phoneNumber must be String' })
+  if (role && typeof role !== 'string')
+    return res.status(400).send({ err: 'role must be String' })
 
   // // Create UpdateBody
   // let updateBody = {}
@@ -211,85 +210,85 @@ const updateUser = expressAsyncHandler(async (req, res) => {
   //   { new: true }
   // )
 
-  let user = await User.findById(userId);
+  let user = await User.findById(userId)
 
   if (channel) {
-    user.channel = channel;
+    user.channel = channel
     await Promotion.updateMany(
-      { "user._id": userId },
-      { "user.channel": channel }
-    );
+      { 'user._id': userId },
+      { 'user.channel': channel }
+    )
   }
 
-  if (userName) user.userName = userName;
+  if (userName) user.userName = userName
 
   if (storeName) {
-    user.storeName = storeName;
+    user.storeName = storeName
     await Promise.all([
       Promotion.updateMany(
-        { "user._id": userId },
-        { "user.storeName": storeName }
+        { 'user._id': userId },
+        { 'user.storeName': storeName }
       ),
       Promotion.updateMany(
         {},
-        { "comments.$[comment].storeName": `${storeName}` },
-        { arrayFilters: [{ "comment.user": userId }] }
+        { 'comments.$[comment].storeName': `${storeName}` },
+        { arrayFilters: [{ 'comment.user': userId }] }
       ),
-    ]);
+    ])
   }
   if (phoneNumber) {
-    user.phoneNumber = phoneNumber;
+    user.phoneNumber = phoneNumber
     await Promotion.updateMany(
-      { "user._id": userId },
-      { "user.phoneNumber": phoneNumber }
-    );
+      { 'user._id': userId },
+      { 'user.phoneNumber': phoneNumber }
+    )
   }
 
   if (userAddress) {
-    user.userAddress = userAddress;
+    user.userAddress = userAddress
     await Promotion.updateMany(
-      { "user._id": userId },
-      { "user.userAddress": userAddress }
-    );
+      { 'user._id': userId },
+      { 'user.userAddress': userAddress }
+    )
   }
 
-  if (userImage) user.userImage = userImage;
+  if (userImage) user.userImage = userImage
 
-  if (role) user.role = role;
+  if (role) user.role = role
 
-  await user.save();
-  return res.send({ user });
-});
+  await user.save()
+  return res.send({ user })
+})
 
 // @desc    Delete a User
 // @route   DELETE   /api/user/:userId
 // @access  Private
 const deleteUser = expressAsyncHandler(async (req, res) => {
-  const { userId } = req.params;
+  const { userId } = req.params
   if (!mongoose.isValidObjectId(userId))
-    res.status(400).send({ err: "invalid userId" });
+    res.status(400).send({ err: 'invalid userId' })
   const [user] = await Promise.all([
     // 유저 삭제
     User.findOneAndDelete({ _id: userId }),
     // 유저가 작성한 프로모션 삭제
-    Promotion.deleteMany({ "user._id": userId }),
+    Promotion.deleteMany({ 'user._id': userId }),
     // 프로모션모델에서 유저가 작성한 코멘트 삭제
     Promotion.updateMany(
-      { "comments.user": userId },
+      { 'comments.user': userId },
       { $pull: { comments: { user: userId } } }
     ),
     // 코멘트모델에서 유저가 작성한 코멘트 삭제
     Comment.deleteMany({ user: userId }),
-  ]);
+  ])
 
-  return res.send({ user });
-});
+  return res.send({ user })
+})
 
 // @desc    Get User Profile
 // @route   GET   /api/user/profile
 // @access  Private
 const getUserProfile = expressAsyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id)
 
   if (user) {
     return res.json({
@@ -300,18 +299,18 @@ const getUserProfile = expressAsyncHandler(async (req, res) => {
       userAddress: user.userAddress,
       userImage: user.userImage,
       role: user.role,
-    });
+    })
   } else {
-    res.status(404);
-    throw new Error("User not found!!");
+    res.status(404)
+    throw new Error('User not found!!')
   }
-});
+})
 
 const getUserProfileWithToken = expressAsyncHandler(async (req, res) => {
-  const data = req.user;
+  const data = req.user
 
-  return res.status(200).send(data);
-});
+  return res.status(200).send(data)
+})
 
 export {
   authUser,
@@ -323,4 +322,4 @@ export {
   getUserProfile,
   getUserProfileWithToken,
   preSigned,
-};
+}
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,51 +1,31 @@
-import jwt from "jsonwebtoken";
-import expressAsyncHandler from "express-async-handler";
-import { User } from "../models/User.js";
+import jwt from 'jsonwebtoken'
+import expressAsyncHandler from 'express-async-handler'
+import { User } from '../models/User.js'
 
-const protect = async (req, res, next) => {
-  console.log("🔥🔥🔥🔥🔥🔥🔥🔥 Protect Middleware Started!!!!!");
+const protect = expressAsyncHandler(async (req, res, next) => {
+  let token
 
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
+    req.headers.authorization.startsWith('Bearer')
   ) {
-    let token = req.headers.authorization.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    try {
+      token = req.headers.authorization.split(' ')[1]
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-    req.user = await User.findById(decoded.id).select("-password");
+      req.user = await User.findById(decoded.id).select('-password')
 
-    return next();
-  } else {
-    return;
+      next()
+    } catch (error) {
+      console.error('authMiddleware', error)
+      res.status(401)
+      throw new Error('Not authorized, token failed !!')
+    }
   }
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized, no token !!!')
+  }
+})
 
-  // if (!token) {
-  //   res.status(401)
-  //   throw new Error('Not authorized, no token !!!')
-  // }
-};
-
-export { protect };
-
-// console.log(
-//   `🔥🔥🔥🔥🔥🔥🔥🔥 req.headers exists: ${req.headers.authorization}`
-// );
-// try {
-//   let token = req.headers.authorization.split(" ")[1];
-
-//   console.log(`🔥🔥🔥🔥🔥🔥🔥🔥🔥 token: ${token}`);
-
-//   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//   console.log(`🔥🔥🔥🔥🔥🔥🔥🔥🔥 decoded: ${decoded}`);
-
-//   req.user = await User.findById(decoded.id).select("-password");
-
-//   console.log(`🔥🔥🔥🔥🔥🔥🔥🔥🔥 req.user: ${req.user}`);
-
-//   next();
-// } catch (error) {
-//   console.error("authMiddleware", error);
-//   res.status(401);
-//   throw new Error("Not authorized, token failed !!");
-// }
+export { protect }
