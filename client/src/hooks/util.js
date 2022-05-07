cfec20d8cb17c5c525ebab9041e51b248451b44b
unknown <mose23@naver.comm>
2022-05-07 15:39:08 +0900

    2

--- client/src/hooks/util.js
+++ client/src/hooks/util.js
@@ -1,4 +1,4 @@
-import * as ImagePicker from "expo-image-picker";
+import * as ImagePicker from 'expo-image-picker'
 
 export const useImageUri = async () => {
   let result = await ImagePicker.launchImageLibraryAsync({
@@ -7,57 +7,57 @@ export const useImageUri = async () => {
     base64: true,
     aspect: [4, 3],
     quality: 1,
-  });
+  })
 
   if (!result.cancelled) {
     return {
       uri: result.uri,
       type:
-        Platform.OS === "android" ? `image/jpeg` || `image/png` : `image/jpg`,
+        Platform.OS === 'android' ? `image/jpeg` || `image/png` : `image/jpg`,
       name: `${result.base64.slice(5, 10)}_${Date.now()}${
-        Platform.OS === "android" ? `.jpeg` || `.png` : `.jpg`
+        Platform.OS === 'android' ? `.jpeg` || `.png` : `.jpg`
       }`,
-    };
+    }
   }
-};
+}
 
 export const usePhoneNumberFormat = (num) => {
-  let formatNum;
+  let formatNum
 
-  formatNum = num?.replace(/(\d{3})(\d{3,4})(\d{4})/, "$1-$2-$3");
+  formatNum = num?.replace(/(\d{3})(\d{3,4})(\d{4})/, '$1-$2-$3')
 
-  return formatNum;
-};
+  return formatNum
+}
 
 export const useCleanUpPhoneNumberForm = (num) => {
-  let formatNum;
+  let formatNum
 
-  formatNum = num?.replace(/-/gi, "");
+  formatNum = num?.replace(/-/gi, '')
 
-  return formatNum;
-};
+  return formatNum
+}
 
 export const useDateFormat = (date) => {
-  const data = new Date(date);
-  const month = data.getMonth() + 1;
-  const day = data.getDate();
+  const data = new Date(date)
+  const month = data.getMonth() + 1
+  const day = data.getDate()
 
-  const result = `${month}월 ${day}일`;
+  const result = `${month}월 ${day}일`
 
-  return result;
-};
+  return result
+}
 
 export const usePromotionDuration = (startDate, endDate) => {
-  const data = new Date(endDate) - new Date(startDate);
-  const result = new Date(data).getDate();
+  const data = new Date(endDate) - new Date(startDate)
+  const result = new Date(data).getDate()
 
-  return `${result}일`;
-};
+  return `${result}일`
+}
 
 export const useExpirationValidation = (today, endDate) => {
   if (new Date(endDate).getTime() > today.getTime()) {
-    return true;
+    return true
   } else {
-    return false;
+    return false
   }
-};
+}
