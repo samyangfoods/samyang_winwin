041b0de624b419d6a011eb945f3a07b81cad5828
moses1206 <33392504+moses1206@users.noreply.github.com>
2022-05-24 07:34:01 +0900

    Docs : hooks/urlSetting.js

--- client/src/hooks/UrlSetting.js
+++ client/src/hooks/UrlSetting.js
@@ -1,15 +1,15 @@
 // Local Setting
-export const basicApiUrl = "http://api.samyangfoods.site/api";
+export const basicApiUrl = 'https://api.samyangfoods.site/api'
 // "http://3.39.151.104/api";
 // Platform.OS === "android"
 //   ? `http://10.0.2.2:${PORT}/api`
 //   : `http://localhost:${PORT}/api`;
 
-export const socketUrl = "http://api.samyangfoods.site/";
+export const socketUrl = 'https://api.samyangfoods.site/'
 // "http://3.39.151.104/";
 // Platform.OS === "android"
 //   ? `http://10.0.2.2:${PORT}`
 //   : `http://localhost:${PORT}`;
 
 export const imageW140 =
-  "https://samyang-bucket.s3.ap-northeast-2.amazonaws.com/raw/";
+  'https://samyang-bucket.s3.ap-northeast-2.amazonaws.com/w140/'
