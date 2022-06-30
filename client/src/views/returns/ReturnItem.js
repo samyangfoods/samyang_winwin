c11779f44cae03bbc04644df74f5823aa113b0b4
moses1206 <33392504+moses1206@users.noreply.github.com>
2022-06-30 18:15:50 +0900

    2

--- client/src/views/returns/ReturnItem.js
+++ client/src/views/returns/ReturnItem.js
@@ -1,9 +1,11 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React, { useEffect, useState, useCallback } from 'react'
+import { View, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { TextInput } from 'react-native-paper'
+import { FloatingLabelInput } from 'react-native-floating-label-input'
 
 export default function ReturnItem({ item, changeReturnValue }) {
   const [returnValue, setReturnValue] = useState('')
+  const [phone, setPhone] = useState('')
 
   // useEffect(() => {
   //   const inputArray = []
@@ -24,14 +26,46 @@ export default function ReturnItem({ item, changeReturnValue }) {
   }
 
   return (
-    <View>
-      <TextInput
-        label={item.productName}
-        value={returnValue}
-        mode='outlined'
-        style={styles.inputStyle}
-        theme={theme}
-        onChange={onChangeEvent}
+    // <View>
+    //    <TextInput
+    //     label={item.productName}
+    //     value={returnValue}
+    //     mode='outlined'
+    //     style={styles.inputStyle}
+    //     theme={theme}
+    //     onChange={onChangeEvent}
+    //   />
+    // </View>
+    <View style={{ padding: 50, flex: 1, backgroundColor: '#fff' }}>
+      <FloatingLabelInput
+        label='Phone'
+        value={phone}
+        staticLabel
+        hintTextColor={'#aaa'}
+        mask='99 (99) 99999-9999'
+        hint='55 (22) 98765-4321'
+        containerStyles={{
+          borderWidth: 2,
+          paddingHorizontal: 10,
+          backgroundColor: '#fff',
+          borderColor: 'blue',
+          borderRadius: 8,
+        }}
+        customLabelStyles={{
+          colorFocused: 'red',
+          fontSizeFocused: 12,
+        }}
+        labelStyles={{
+          backgroundColor: '#fff',
+          paddingHorizontal: 5,
+        }}
+        inputStyles={{
+          color: 'blue',
+          paddingHorizontal: 10,
+        }}
+        onChangeText={(value) => {
+          setPhone(value)
+        }}
       />
     </View>
   )
