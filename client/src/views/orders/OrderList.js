cd709c5fd8d77912a0813f3971c100a0c9b40979
phainestha1 <250wons@gmail.com>
2022-07-04 10:31:15 +0900

    Add: OrderList.js and its navigation button

---
+++ client/src/views/orders/OrderList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BasicContainer, Text } from "../../styles/Style";
+
+const OrderList = () => {
+  return (
+    <BasicContainer>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+      <Text>Hello</Text>
+    </BasicContainer>
+  );
+};
+
+export default OrderList;
